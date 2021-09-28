3d28ac669563f419b115d5a26c227a62db5a6c2a
Alex-Pozos <alejandro.po.ra@gmail.com>
2021-09-28 07:09:03 -0500

    MasPorNosotros

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,54 +10,46 @@
         <div class="section-title text-center">
             <span>Numeralia</span>
         </div>
-    </div>
-    <div>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-    </div>
-    <div id="projectFacts" class="sectionClass">
-        <div class="fullwidth height columns">
-            <div class="factswrap" onscroll="myFunction()">
-                <div class="item fadeupbig animated animated" data-number="450" style="visibility: visible;">
-                    <img src="img/networking.png" alt="">
-                    <p id="number1" class="number">0</p>
-                    <span class="Line-under"></span>
-                    <p>Lorem</p>
-                </div>
-                <div class="item fadeupbig animated animated" data-number="20" style="visibility: visible;">
-                    <img src="img/portfolio.png" alt="">
-                    <p id="number2" class="number">0</p>
-                    <span class="Line-under"></span>
-                    <p>Lorem</p>
-                </div>
-                <div class="item fadeupbig animated animated" data-number="700" style="visibility: visible;">
-                    <img src="img/handshake.png" alt="">
-                    <p id="number3" class="number">0</p>
-                    <span class="Line-under"></span>
-                    <p>Lorem</p>
-                </div>
-                <div class="item fadeupbig animated animated" data-number="8" style="visibility: visible;">
-                    <img src="img/company.png" alt="">
-                    <p id="number4" class="number">0</p>
-                    <span class="Line-under"></span>
-                    <p>Lorem</p>
+        <div class="row">
+            <div class="col">
+                <img src="{{ asset('img/index/comunidad.png') }}" alt="">
+            </div>
+            <div class="col">
+                <h1 class="display-4" style="color:#004b87">Alcance comunitario</h1>
+            </div>
+        </div>
+        <div id="projectFacts" class="sectionClass">
+            <div class="fullwidth height columns">
+                <div class="factswrap" onscroll="myFunction()">
+                    <div class="item fadeupbig animated animated" data-number="450" style="visibility: visible;">
+                        <img src="img/networking.png" alt="">
+                        <p id="number1" class="number">0</p>
+                        <span class="Line-under"></span>
+                        <p>Lorem</p>
+                    </div>
+                    <div class="item fadeupbig animated animated" data-number="20" style="visibility: visible;">
+                        <img src="img/portfolio.png" alt="">
+                        <p id="number2" class="number">0</p>
+                        <span class="Line-under"></span>
+                        <p>Lorem</p>
+                    </div>
+                    <div class="item fadeupbig animated animated" data-number="700" style="visibility: visible;">
+                        <img src="img/handshake.png" alt="">
+                        <p id="number3" class="number">0</p>
+                        <span class="Line-under"></span>
+                        <p>Lorem</p>
+                    </div>
+                    <div class="item fadeupbig animated animated" data-number="8" style="visibility: visible;">
+                        <img src="img/company.png" alt="">
+                        <p id="number4" class="number">0</p>
+                        <span class="Line-under"></span>
+                        <p>Lorem</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
     <div>
         <br>
         <br>
